293f448dab9878507591d9d07469d1f323a114d1
Trajaa <rajaa.tiane1@gmail.com>
2023-03-28 13:54:04 +0000

    concurrent.futures

--- main.py
+++ main.py
@@ -1,55 +1,34 @@
 from getters import *
 import csv
+import concurrent.futures
 import time
-import multiprocessing
+
+start = time.time()
+
+# Nom du fichier horodaté
+path = getFileInfo("path")
+filename = "%s\%s_%s.csv" % (path, getFileInfo("genre"), time.strftime(getFileInfo("dateFormat")))
+
 # les colonnes
 section = sectionName
 fieldnames = getkeys(section[0])
+
 maxRecords = int(getFileInfo("maxRecords"))
-num_processes = multiprocessing.cpu_count()
-# Fonction qui génère les données pour une plage donnée
-def generate_data(start, end, fieldnames):
-    data = []
-    for j in range(start, end):
-        data.append([getData(fieldname) for fieldname in fieldnames])
-    return data
 
-if __name__ == '__main__':
-    start = time.time()
+# ouvrir le fichier csv en mode écriture
+with open(filename, 'w', newline='') as csvfile:
+    # créer un écrivain csv
+    writer = csv.writer(csvfile,delimiter=getFileInfo("delimiter"))
 
-    # Nom du fichier horodaté
-    path = getFileInfo("path")
-    filename = "%s\%s_%s.csv" % (path, getFileInfo("genre"), time.strftime(getFileInfo("dateFormat")))
+    # écrire les en-têtes de colonne
+    writer.writerow(fieldnames)
 
-
-
-    # diviser le travail en plusieurs tranches
-    slice_size = maxRecords // num_processes
-    slices = [(i * slice_size, (i + 1) * slice_size) for i in range(num_processes)]
-    if maxRecords % num_processes != 0:
-        slices.append((num_processes * slice_size, maxRecords))
-
-    # utiliser multiprocessing pour générer les données en parallèle
-    with multiprocessing.Pool(processes=num_processes) as pool:
-        results = []
-        for s in slices:
-            results.append(pool.apply_async(generate_data, args=(s[0]+1, s[1]+1, fieldnames)))
-        data = []
-        for r in results:
-            data += r.get()
-
-    # ouvrir le fichier csv en mode écriture
-    with open(filename, 'w', newline='') as csvfile:
-        # créer un écrivain csv
-        writer = csv.writer(csvfile, delimiter=getFileInfo("delimiter"))
-
-        # écrire les en-têtes de colonne
-        writer.writerow(fieldnames)
-
-        # écrire les données générées
-        for row in data:
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        for j in range(1, maxRecords + 1):
+            # utiliser executor pour exécuter getData() en parallèle
+            row = executor.map(getData, fieldnames)
             writer.writerow(row)
 
-    end = time.time()
-    print("le nbr de ligne est "+ str(maxRecords)+" lignes")
-    print("csv généré en " + str(end - start) + " secondes")
+end = time.time()
+print("le nbr de ligne est "+ str(maxRecords)+" lignes")
+print("csv généré en " + str(end - start) + " secondes")
