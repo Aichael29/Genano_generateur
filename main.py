f69a11c8fbeb7f59bbfad7dd1b6cf1e127c7d256
aichael29 <aichaelfelchaoui29@gmail.com>
2023-03-28 13:44:02 +0000

    multithreading

--- main.py
+++ main.py
@@ -1,25 +1,57 @@
-from getters import *
+from getters import getFileInfo, getkeys, getData, sectionName, time
 import csv
+import threading
 
-start = time.time()
+star = time.time()
+# Fonction pour écrire les données dans le fichier csv
+def write_data(start, end, writer):
+    for i in range(start, end):
+        writer.writerow(getData(fieldname) for fieldname in fieldnames)
+
+# Récupérer le nombre de threads à utiliser en fonction du nombre de lignes de données
+maxRecords = int(getFileInfo("maxRecords"))
+if maxRecords <= 100000:
+    num_threads = 1
+elif maxRecords <= 200000:
+    num_threads = 2
+elif maxRecords <= 300000:
+    num_threads = 3
+elif maxRecords <= 500000:
+    num_threads = 4
+elif maxRecords <= 1000000:
+    num_threads = 5
+# Et ainsi de suite, en fonction du nombre de lignes de données en sortie
 
 # Nom du fichier horodaté
 path = getFileInfo("path")
 filename = "%s\%s_%s.csv" % (path, getFileInfo("genre"), time.strftime(getFileInfo("dateFormat")))
 
-# les colonnes
+# Les colonnes
 fieldnames = getkeys(sectionName[0])
-maxRecords = int(getFileInfo("maxRecords"))
 
-# ouvrir le fichier csv en mode écriture
+# Ouvrir le fichier csv en mode écriture
 with open(filename, 'w', newline='') as csvfile:
-    # créer un écrivain csv
+    # Créer un écrivain csv
     writer = csv.writer(csvfile,delimiter=getFileInfo("delimiter"))
-    # écrire les en-têtes de colonne
+    # Écrire les en-têtes de colonne
     writer.writerow(fieldnames)
-    for j in range(1, maxRecords + 1):
-        writer.writerow(getData(fieldname) for fieldname in fieldnames)
 
+    # Diviser le travail en plusieurs threads
+    threads = []
+    for i in range(num_threads):
+        start = i * (maxRecords // num_threads)
+        end = (i + 1) * (maxRecords // num_threads)
+        if i == num_threads - 1:
+            end = maxRecords
+        t = threading.Thread(target=write_data, args=(start, end, writer))
+        threads.append(t)
+        t.start()
+
+    # Attendre que tous les threads aient fini
+    for t in threads:
+        t.join()
+
+# Afficher le temps d'exécution et le nombre de lignes de données en sortie
 end = time.time()
-print("le nbr de ligne est "+ str(maxRecords)+" lignes")
-print("csv généré en " + str(end - start) + " secondes")
+print("Le nombre de lignes est " + str(maxRecords) + " lignes")
+print("CSV généré en " + str(end - star) + " secondes")
