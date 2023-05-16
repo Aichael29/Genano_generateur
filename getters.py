2933de7f0b053563fda8dda4158bd2b3b7d85056
dell latitude 7400 <aichaelfelchaoui29@gmail.com>
2023-05-16 16:45:11 +0200

    dependance

--- getters.py
+++ getters.py
@@ -51,7 +51,7 @@ def typeapproved(inData, dtype):
     return valid_values
 
 
-values = [str(i) for i in getvalues(sectionName[0], 'id')]
+values = [str(i) for i in getvalues(sectionName[0], 'id_date')]
 currentId = 0
 sectionName=[section for section in data.sections()]
 
@@ -121,23 +121,108 @@ def getRandomBoolean(val,form=""):
     except:
         sys.exit("erreur boolean")
 
-# les types possibles
+def getIncrementalString(start, form=""):
+    try:
+        global currentId
+        currentId += 1
+        return start + str(currentId)
+    except:
+        sys.exit("erreur chaine de caractère + nombre autoincrement")
+
+# Add the new type to the dataTypes dictionary
 dataTypes = {
     'int': getRandomInt,
     'str': getRandomString,
     'timestamp': getTimestamp,
     'float': getRandomFloat,
     "datetime": getRandomDate,
-    "bool":getRandomBoolean,
-    "mdn":getRandomPhone,
-    "autoicrement":getCurrentId
+    "bool": getRandomBoolean,
+    "mdn": getRandomPhone,
+    "autoincrement": getCurrentId,
+    "chaine_autoincrement": getIncrementalString
 }
 
-# fonction pour obtenir/generer les ranges
+
 def getData(fieldname):
-    section=sectionName
-    field = getvalues(section[0], fieldname)
+    section = sectionName[0]
+    field = getvalues(section, fieldname)
     dtype = field[0]
+
+    # Traitement du champ 'dn'
+    if fieldname == 'dn':
+        type_reseau = getvalues(section, 'type_reseau')[0]
+        type_destination = getvalues(section, 'type_destination')[0]
+
+        if type_reseau == 'fix':
+            field[2] = "2125" + str(random.randint(10000000, 99999999))
+        else:
+            field[2] = "2126" + str(random.randint(10000000, 99999999))
+
+        if type_destination == 'international':
+            field[2] = "000" + str(random.randint(100000000, 999999999))
+
+    # Traitement du champ 'type_even'
+    if fieldname == 'type_even':
+        type_even = field[2]
+        marche = getvalues(section, 'marche')[2]
+
+        if type_even == 'voice' and marche == 'home':
+            # Effectuer l'action souhaitée lorsque type_even est "voice" et marche est "home"
+            pass
+
+    # Traitement du champ 'even_minutes'
+    if fieldname == 'even_minutes':
+        type_even = getvalues(section, 'type_even')[2]
+
+        if type_even == 'sms':
+            field[2] = ''
+
+    # Traitement du champ 'type_destination'
+    if fieldname == 'type_destination':
+        type_destination = field[2]
+
+        if type_destination == 'international':
+            field[2] = 'International'
+
+    # Traitement du champ 'operator'
+    if fieldname == 'operator':
+        operator = field[2]
+        country = getvalues(section, 'country')[2]
+
+        if operator == 'International':
+            zones = ['zone1', 'zone2', 'zone3', 'zone4']
+            field[2] = random.choice(zones)
+
+        if country != 'Morocco':
+            field[2] = 'International'
+
+    # Traitement du champ 'billing_type'
+    if fieldname == 'billing_type':
+        billing_type = field[2]
+
+        if billing_type == 'prepaid':
+            field[2] = 'mobile prepaid'
+
+    # Traitement du champ 'segment'
+    if fieldname == 'segment':
+        segment = field[2]
+
+        if segment == 'B2B':
+            field[2] = 'prepaid'
+
+    # Traitement du champ 'date_debut'
+    if fieldname == 'date_debut':
+        end = getData('date_fin')
+        field[2] = getTimestamp([field[2], end], form=field[1])
+
+    # Traitement du champ 'date_fin'
+    if fieldname == 'date_fin':
+        start = getData('date_debut')
+        field[2] = getTimestamp([start, field[2]], form=field[1])
+
     if dtype in dataTypes:
         return dataTypes[dtype](field[2], form=field[1])
+
     return field[2]
+
+
