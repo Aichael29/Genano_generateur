4e8bfd47f47887d3fbbdce6a18da9c3c9fe56624
dell latitude 7400 <aichaelfelchaoui29@gmail.com>
2023-05-24 16:17:14 +0200

    derniere modif

--- main.py
+++ main.py
@@ -1,25 +1,104 @@
-from getters import *
 import csv
+import random
+from datetime import datetime, timedelta
 
-start = time.time()
+def generate_csv_file(num_rows):
+    columns = [
+        "id_date", "dn", "date_debut", "type_even", "nombre_even", "even_minutes", "direction_appel",
+        "termination_type", "type_reseau", "type_destination", "operator", "zone", "country",
+        "profile_id", "city", "region", "gamme", "marche", "segment", "billing_type", "contract_id", "date_fin"
+    ]
 
-# Nom du fichier horodaté
-path = getFileInfo("path")
-filename = "%s\%s_%s.csv" % (path, getFileInfo("genre"), time.strftime(getFileInfo("dateFormat")))
+    rows = []
+    for _ in range(num_rows):
+        row = []
+        # id_date
+        id_date = random_date(datetime(2022, 1, 1), datetime(2023, 3, 31))
+        row.append(id_date.strftime("%Y%m%d"))
+        # dn
+        dn = generate_dn()
+        row.append(dn)
+        # date_debut
+        row.append(id_date.strftime("%Y-%m-%d %H:%M:%S"))
+        # type_even
+        type_even = random.choice(["voice", "sms"])
+        row.append(type_even)
+        # nombre_even
+        row.append(random.randint(1, 10))
+        # even_minutes
+        if type_even == "sms":
+            row.append(None)
+            date_fin = id_date + timedelta(minutes=random.randint(1, 60))
+        else:
+            even_minutes = random.randint(1, 60)
+            row.append(even_minutes)
+            date_fin = id_date + timedelta(minutes=even_minutes)
+        # direction_appel
+        row.append(random.choice(["IN", "OUT"]))
+        # termination_type
+        row.append(random.choice(["on-net", "off-net"]))
+        # type_reseau
+        row.append(random.choice(["mobile", "fix"]))
+        # type_destination
+        type_destination = random.choice(["national", "international"])
+        row.append(type_destination)
+        # operator
+        if type_destination == "international":
+            row.append("International")
+        else:
+            row.append(random.choice(["INWI", "IAM", "ORANGE"]))
+        # zone
+        if row[-1] == "International":
+            row.append(random.choice(["zone1", "zone2", "zone3", "zone4"]))
+        else:
+            row.append(random.choice(["Zone1", "Zone2", "Zone3", "Zone4", "Nationale Zone"]))
+        # country
+        if random.random() < 0.2:  # 20% chance of non-Maroc country
+            row.append(random.choice(["Morocco", "France", "Spain", "USA"]))
+        else:
+            row.append("Morocco")
+        # profile_id
+        row.append(str(random.randint(1000, 9999)))
+        # city
+        row.append(random.choice(["Casablanca", "Rabat", "Marrakech", "Fes", "Tangier"]))
+        # region
+        row.append(random.choice(["Grand Casablanca", "Rabat-Sale-Kenitra", "Marrakech-Safi", "Fes-Meknes", "Tanger-Tetouan-Al Hoceima"]))
+        # gamme
+        row.append(random.choice(["Fibre optique", "ADSL", "MRE", "Data Prepaid", "Data Postpaid", "Forfaits 99 dhs",
+                                  "Forfaits 49 dhs", "Forfaits 149 dhs", "Forfaits 199 dhs", "Forfaits 249 dhs"]))
+        # marche
+        if row[-2] == "B2B":
+            row.append("mobile postpaid home")
+        else:
+            row.append(random.choice(["Mobile Prepaid", "Mobile Postpaid", "Home"]))
+        # segment
+        row.append(random.choice(["B2B", "B2C", "Autres"]))
+        # billing_type
+        if row[-1] == "B2B":
+            row.append("Prepaid")
+        else:
+            row.append(random.choice(["Prepaid", "Postpaid", "Autres"]))
+        # contract_id
+        row.append("ABC" + str(random.randint(100000, 999999)))
+        # date_fin
+        row.append(date_fin.strftime("%Y-%m-%d %H:%M:%S"))
 
-# les colonnes
-fieldnames = getkeys(sectionName[0])
-maxRecords = int(getFileInfo("maxRecords"))
+        rows.append(row)
 
-# ouvrir le fichier csv en mode écriture
-with open(filename, 'w', newline='') as csvfile:
-    # créer un écrivain csv
-    writer = csv.writer(csvfile,delimiter=getFileInfo("delimiter"))
-    # écrire les en-têtes de colonne
-    writer.writerow(fieldnames)
-    for j in range(1, maxRecords + 1):
-        writer.writerow(getData(fieldname) for fieldname in fieldnames)
+    with open("data.csv", "w", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(columns)
+        writer.writerows(rows)
 
-end = time.time()
-print("le nbr de ligne est "+ str(maxRecords)+" lignes")
-print("csv généré en " + str(end - start) + " secondes")
+    print(f"CSV file with {num_rows} rows generated successfully!")
+
+def random_date(start_date, end_date):
+    return start_date + timedelta(seconds=random.randint(0, int((end_date - start_date).total_seconds())))
+
+def generate_dn():
+    prefix = "2125" if random.random() < 0.5 else "2126"
+    suffix = str(random.randint(10000000, 99999999))
+    return prefix + suffix
+
+# Exemple d'utilisation
+generate_csv_file(10)  # Génère un fichier CSV avec 10 lignes
