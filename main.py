0ee4e1bcb75a211a9801342334fc95775a1ef75d
dell latitude 7400 <aichaelfelchaoui29@gmail.com>
2023-06-06 22:53:21 +0200

    done

--- main.py
+++ main.py
@@ -41,21 +41,25 @@ def generate_csv_file(num_rows):
         # termination_type
         row.append(random.choice(["on-net", "off-net"]))
         # type_reseau
-        row.append(random.choice(["mobile", "fix"]))
+        type_reseau=random.choice(["mobile", "fix"])
+        row.append(type_reseau)
         # type_destination
         type_destination = random.choice(["national", "international"])
-        row.append(type_destination)
+        choices = ["national", type_destination]
+        weights = [0.7, 0.3]
+        row.append(random.choices(choices, weights)[0])
         # operator
-        if type_destination == "international":
+        if type_destination == "national":
+            row.append(random.choice(["INWI", "IAM", "ORANGE"]))
+        elif type_destination == "international":
             row.append("International")
-        else:
-            row.append(random.choice(["INWI", "IAM", "ORANGE"])
-)
+
         # country
-        if type_destination == "national" : # 20% chance of non-Maroc country
+        if type_destination == "national":
             row.append("Morocco")
         else:
             row.append(random.choice(["France", "Spain", "USA"]))
+
         # profile_id
         row.append(str(random.randint(1000, 9999)))
         # city
@@ -64,23 +68,48 @@ def generate_csv_file(num_rows):
         else:
             row.append("international city")
         # gamme
-        row.append(random.choice(["Fibre optique", "ADSL", "MRE", "Data Prepaid", "Data Postpaid", "Forfaits 99 dhs",
-                       "Forfaits 49 dhs", "Forfaits 149 dhs", "Forfaits 199 dhs", "Forfaits 249 dhs"]))
+        gamme=random.choice(["Fibre optique", "ADSL", "MRE", "Data Prepaid", "Data Postpaid", "Forfaits 99 dhs",
+                       "Forfaits 49 dhs", "Forfaits 149 dhs", "Forfaits 199 dhs", "Forfaits 249 dhs"])
+        row.append(gamme)
+
+        # marche
+        marche=random.choice(["Mobile Prepaid", "Mobile Postpaid","Home"])
+        if type_reseau == "mobile":
+            row.append(random.choice(["Mobile Prepaid", "Mobile Postpaid"]))
+        elif type_reseau == "Fix":
+            row.append(random.choice(["Home", "Mobile Postpaid"]))
+        elif gamme == "Data Prepaid":
+            row.append("Mobile Prepaid")
+        elif gamme == "Data Postpaid":
+            row.append("Mobile Postpaid")
+        elif gamme == "MRE":
+            row.append(random.choice(["Mobile Prepaid", "Mobile Postpaid"]))
+        elif gamme == "ADSL" or gamme == "Fibre optique":
+            row.append("Home")
+        else:
+            row.append(marche)
+
         # segment
         segment = random.choice(["B2B", "B2C", "Autres"])
-        row.append(segment)
-        # marche
-        if segment == "B2B":
-            row.append("Mobile Postpaid")
+        if marche == "Mobile Prepaid":
+            choices = ["B2C", segment]
+            weights = [0.99, 0.01]  # Les poids déterminent la probabilité de chaque choix
+            row.append(random.choices(choices, weights)[0])
+        elif marche == "Mobile Postpaid":
+            choices = ["B2B", segment]
+            weights = [0.99, 0.01]  # Les poids déterminent la probabilité de chaque choix
+            row.append(random.choices(choices, weights)[0])
         else:
-            row.append(random.choice(["Mobile Prepaid", "Mobile Postpaid", "Home"])
-)
+            row.append(segment)
+
         # billing_type
-        if segment == "B2B":
-            row.append("Postpaid")
+        if marche == "Mobile Postpaid":
+            row.append("postpaid")
+        elif marche == "Mobile Prepaid":
+            row.append("prepaid")
         else:
-            row.append(random.choice(["Prepaid", "Postpaid", "Autres"])
-)
+            row.append("Autres")
+
         # contract_id
         row.append("ABC" + str(random.randint(100000, 999999)))
         # date_fin
@@ -88,7 +117,7 @@ def generate_csv_file(num_rows):
 
         rows.append(row)
 
-    with open("Traffic.csv", "w", newline="") as csvfile:
+    with open("Traffic data.csv", "w", newline="") as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(columns)
         writer.writerows(rows)
