3e02756d2f578b45ec00380e9a41aa458cca4339
Trajaa <rajaa.tiane1@gmail.com>
2023-06-07 16:05:42 +0100

    version final de multiprocessing

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
-from getters import *
 import csv
 import time
+from getters import *
+from multiprocessing import Pool
 import multiprocessing
-# les colonnes
-section = sectionName
-fieldnames = getkeys(section[0])
-maxRecords = int(getFileInfo("maxRecords"))
-num_processes = multiprocessing.cpu_count()
-# Fonction qui génère les données pour une plage donnée
-def generate_data(start, end, fieldnames):
-    data = []
-    for j in range(start, end):
-        data.append([getData(fieldname) for fieldname in fieldnames])
-    return data
+def generate_row(fieldnames):
+    return [getData(fieldname) for fieldname in fieldnames]
 
 if __name__ == '__main__':
     start = time.time()
@@ -21,35 +13,24 @@ if __name__ == '__main__':
     path = getFileInfo("path")
     filename = "%s\%s_%s.csv" % (path, getFileInfo("genre"), time.strftime(getFileInfo("dateFormat")))
 
-
-
-    # diviser le travail en plusieurs tranches
-    slice_size = maxRecords // num_processes
-    slices = [(i * slice_size, (i + 1) * slice_size) for i in range(num_processes)]
-    if maxRecords % num_processes != 0:
-        slices.append((num_processes * slice_size, maxRecords))
-
-    # utiliser multiprocessing pour générer les données en parallèle
-    with multiprocessing.Pool(processes=num_processes) as pool:
-        results = []
-        for s in slices:
-            results.append(pool.apply_async(generate_data, args=(s[0]+1, s[1]+1, fieldnames)))
-        data = []
-        for r in results:
-            data += r.get()
+    # les colonnes
+    fieldnames = getkeys(sectionName[0])
+    maxRecords = int(getFileInfo("maxRecords"))
 
     # ouvrir le fichier csv en mode écriture
     with open(filename, 'w', newline='') as csvfile:
         # créer un écrivain csv
         writer = csv.writer(csvfile, delimiter=getFileInfo("delimiter"))
-
         # écrire les en-têtes de colonne
         writer.writerow(fieldnames)
+        num_processes = multiprocessing.cpu_count()
+        # Create a multiprocessing pool with the number of desired processes
+        pool = Pool(processes=num_processes)  # You can adjust the number of processes as needed
 
-        # écrire les données générées
-        for row in data:
-            writer.writerow(row)
+        # Generate rows using multiprocessing and write them to the CSV file
+        results = pool.map(generate_row, [fieldnames] * maxRecords)
+        writer.writerows(results)
 
     end = time.time()
-    print("le nbr de ligne est "+ str(maxRecords)+" lignes")
+    print("le nbr de ligne est " + str(maxRecords) + " lignes")
     print("csv généré en " + str(end - start) + " secondes")
