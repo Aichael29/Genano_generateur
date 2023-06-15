96ac28c5633c2b744e7009903ab02121f5e69925
Trajaa <rajaa.tiane1@gmail.com>
2023-06-15 17:04:15 +0100

    ajout des conditions

--- fct.py
+++ fct.py
@@ -7,11 +7,13 @@ import csv
 config = configparser.ConfigParser()
 config.read('config.conf')
 
+
 # Fonction pour générer un numéro de téléphone aléatoire
 def generate_phone_number():
     prefix = config.get('data', 'phone_prefix')
     number = ''.join(random.choice('0123456789') for _ in range(9))
     return prefix + number
+
 
 # Fonction pour générer une date aléatoire
 def generate_date():
@@ -21,6 +23,7 @@ def generate_date():
     random_days = random.randint(0, delta)
     return (start_date + timedelta(days=random_days)).strftime('%Y%m%d')
 
+
 # Fonction pour générer un timestamp aléatoire
 def generate_timestamp():
     start_timestamp = datetime.strptime(config.get('data', 'start_timestamp'), '%Y-%m-%d %H:%M:%S')
@@ -29,13 +32,16 @@ def generate_timestamp():
     random_seconds = random.uniform(0, delta)
     return (start_timestamp + timedelta(seconds=random_seconds)).strftime('%Y-%m-%d %H:%M:%S')
 
+
 # Fonction pour générer une durée aléatoire en minutes
 def generate_minutes():
     return round(random.uniform(0, config.getfloat('data', 'max_minutes')), 2)
 
+
 # Fonction pour générer une valeur aléatoire à partir d'une liste
 def generate_value_from_list(values):
     return random.choice(values)
+
 
 # Fonction pour générer la table de données
 def generate_data_table(num_rows):
@@ -51,23 +57,31 @@ def generate_data_table(num_rows):
             'direction_appel': generate_value_from_list(config.get('data', 'call_directions').split(',')),
             'termination_type': generate_value_from_list(config.get('data', 'termination_types').split(',')),
             'type_reseau': generate_value_from_list(config.get('data', 'network_types').split(',')),
-            'type_destination': generate_value_from_list(config.get('data', 'destination_types').split(',')),
-            'operator': generate_value_from_list(config.get('data', 'operators').split(',')),
+            'type_destination': '',
+            'operator': '',
             'country': '',
-            'profile_id': generate_value_from_list(config.get('data', 'profile_ids').split(',')),
+            'profile_id': random.randint(1000, 9999),
             'city': '',
-            'gamme': generate_value_from_list(config.get('data', 'gammes').split(',')),
+            'gamme': '',
             'marche': '',
-            'segment': generate_value_from_list(config.get('data', 'segments').split(',')),
+            'segment':'',
             'billing_type': '',
             'contract_id': '',
             'last_timestamp': ''
         }
+        if row['termination_type'] == 'on-net':
+            row['operator'] = 'NATIONAL'
+            row['type_destination'] = 'national'
+            row['operator'] = 'INWI'
+        else:
+            row['operator'] = generate_value_from_list(config.get('data', 'operators').split(','))
 
         if row['operator'] == 'INTERNATIONAL':
-            row['country'] = generate_value_from_list([c for c in config.get('data', 'country').split(',') if c != 'Morocco'])
+            row['type_destination'] = 'international'
+            row['country'] = generate_value_from_list([c for c in config.get('data', 'country').split(',') ])
             row['city'] = 'international city'
         else:
+            row['type_destination'] = 'national'
             row['country'] = 'Morocco'
             row['city'] = generate_value_from_list(config.get('data', 'city').split(','))
 
@@ -79,27 +93,41 @@ def generate_data_table(num_rows):
         elif row['type_even'] == 'sms':
             row['even_minutes'] = 0
 
-        row['last_timestamp'] = (datetime.strptime(row['date_debut'], '%Y-%m-%d %H:%M:%S') + timedelta(minutes=row['even_minutes'])).strftime('%Y-%m-%d %H:%M:%S')
+        row['last_timestamp'] = (datetime.strptime(row['date_debut'], '%Y-%m-%d %H:%M:%S') + timedelta(
+            minutes=row['even_minutes'])).strftime('%Y-%m-%d %H:%M:%S')
         row['id_date'] = row['date_debut'].split()[0].replace('-', '')
-
-        # Ajout des conditions pour la gamme et le type de facturation
+        if row['type_reseau'] == 'fix':
+            row['gamme'] = random.choice(['Fibre optique', 'ADSL'])
+        if row['type_reseau'] == 'mobile':
+            row['gamme'] = generate_value_from_list(config.get('data', 'gammes').split(','))
         if row['gamme'] == 'Fibre optique' or row['gamme'] == 'ADSL':
             row['marche'] = 'home'
-            row['billing_type'] = 'fix'
         elif row['gamme'] == 'Data Postpaid':
-            row['marche'] = 'mobil postpaid'
-            row['billing_type'] = 'postpaid'
-        elif row['gamme'] == 'Data Prepaid':
-            row['marche'] = 'mobil prepaid'
+            row['marche'] = 'Mobile Postpaid'
+        elif row['gamme'] == 'MRE':
+            row['marche'] = random.choice(['Mobile Prepaid', 'Mobile Postpaid'])
+        elif row['gamme'] == 'Data Prepaid' :
+            row['marche'] = 'Mobile Prepaid'
+
+        else:
+            row['marche']= generate_value_from_list(config.get('data', 'marches').split(','))
+
+        if row['marche'] == 'Mobile Prepaid':
             row['billing_type'] = 'prepaid'
         else:
-            row['marche'] = generate_value_from_list(config.get('data', 'marches').split(','))
-            row['billing_type'] = generate_value_from_list(config.get('data', 'billing_types').split(','))
-        # Ajout de l'identifiant de contrat
+            row['billing_type'] = 'postpaid'
+        if row['billing_type'] == 'prepaid':
+            row['segment'] = 'B2B' if random.random() < 0.1 else 'B2C'
+        elif row['billing_type'] == 'postpaid':
+            row['segment'] = 'B2B' if random.random() < 0.1 else 'B2C'
+
+
+
         row['contract_id'] = config.get('data', 'contract_id_prefix') + str(random.randint(0, config.getint('data', 'max_contract_id')))
 
         data_table.append(row)
     return data_table
+
 
 # Fonction pour sauvegarder la table de données dans un fichier CSV
 def save_data_to_csv(data_table, filename):
@@ -110,3 +138,8 @@ def save_data_to_csv(data_table, filename):
         writer.writerows(data_table)
 
 
+# Génération des données et sauvegarde dans un fichier CSV
+num_rows = config.getint('data', 'num_rows')
+data_table = generate_data_table(num_rows)
+output_file = config.get('data', 'output_file')
+save_data_to_csv(data_table, output_file)
